814b43b5f811a3234c05adeca7fd6fbb890f1c63
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-03-13 04:34:02 -0300

    update banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,12 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 function Banner() {
     return (
         <div className="relative">
+            <div className="absolute w-full h-32 bg-gradient-to-t from-gray-100 to-transparent bottom-0 z-20" />
             <Carousel
                 autoPlay
                 infiniteLoop
                 showStatus={false}
-                showIndicators={false} 
+                showIndicators={false}
                 showThumbs={false}
                 interval={5000}
             >
