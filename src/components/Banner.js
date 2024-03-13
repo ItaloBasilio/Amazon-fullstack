881f6e336485f471e7eba3716cd991ec2ae9c408
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-03-13 04:19:31 -0300

    update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,9 +1,31 @@
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+
 function Banner() {
-  return (
-    <div>
-        
-    </div>
-  )
+    return (
+        <div className="relative">
+            <Carousel
+                autoPlay
+                infiniteLoop
+                showStatus={false}
+                showIndicators={false} 
+                showThumbs={false}
+                interval={5000}
+            >
+                <div>
+                    <img loading="lazy" src="https://links.papareact.com/gi1" alt="" />
+                </div>
+
+                <div>
+                    <img loading="lazy" src="https://links.papareact.com/6ff" alt="" />
+                </div>
+                <img loading="lazy" src="https://links.papareact.com/7ma" alt="" />
+                <div>
+
+                </div>
+            </Carousel>
+        </div>
+    )
 }
 
 export default Banner
