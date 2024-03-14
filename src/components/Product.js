6dfd816ba651f3cc45db27962dc78c91b954921c
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-03-14 04:10:23 -0300

    CSS description

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,6 +1,7 @@
-import { StarIcon } from "@heroicons/react/outline";
+import { StarIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { useState } from "react";
+import Currency from "react-currency-formatter";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
@@ -10,9 +11,11 @@ function Product({ id, title, price, description, category, image }) {
     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
   );
 
+  const [hasPrime] = useState(Math.random() < 0.5);
+
   return (
-    <div>
-      <p>{category}</p>
+    <div className="relative flex flex-col m-5 bg-white z-30 p-10">
+      <p className="absolute top-2 right-2 text-xs italic text-gray-400">{category}</p>
 
       <Image src={image} height={200} width={200} objectFit="contain" />
 
@@ -20,12 +23,26 @@ function Product({ id, title, price, description, category, image }) {
 
       <div className="flex">
         {Array(rating)
-        .fill()
-        .map( (_, i) => (
-            <StarIcon className="h-5" />
-        ))}
-        
+          .fill()
+          .map((_, i) => (
+            <StarIcon className="h-5 text-yellow-400 " />
+          ))}
       </div>
+
+      <p className="text-xs my-2 line-clamp-2">{description}</p>
+
+      <div>
+        <Currency quantity={price} currency="USD" />
+      </div>
+
+      {hasPrime && (
+        <div>
+            <img src="https://m.media-amazon.com/images/G/01/support_images/GUID-A5E374A8-16DA-4B39-8E3F-3F3B34E831FB=2=pt-BR=Normal.png" width={150} alt="" />
+            <p>FREE Next-day Delivery</p>
+        </div>
+      )}
+
+      <button>Add to Basket</button>
     </div>
   );
 }
--- src/components/ProductFeed.js
+++ src/components/ProductFeed.js
@@ -3,7 +3,7 @@ import Product from "./Product"
 function ProductFeed({products}) {
   return (
     <div>
-        <h1>Products here</h1>
+        
         {products.map(({ id , title , price , description , category , image }) => (
           <Product 
             key={id}
