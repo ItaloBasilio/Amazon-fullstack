6e479b30473ec57f41e855410dc5ee5c461015f3
ItaloBasilio <italobasiliodasilva@gmail.com>
2024-03-14 23:25:34 -0300

    auth firebase autentication

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDkGgQleY_Qd_Omx1ZK1pJnTTJOQKzWZe0",
+    authDomain: "ecommerce-5ed18.firebaseapp.com",
+    projectId: "ecommerce-5ed18",
+    storageBucket: "ecommerce-5ed18.appspot.com",
+    messagingSenderId: "33419909537",
+    appId: "1:33419909537:web:fd181bfe0d12a16af9d3e0"
+  };
